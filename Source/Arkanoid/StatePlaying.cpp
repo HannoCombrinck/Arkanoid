57ac3d2c2fcea517eaba41da6bd2bf0c4d5ccd77
Hanno <hcombrinck@gmail.com>
2017-11-11 23:01:25 +0200

    Added test code to StatePlaying

--- Source/Arkanoid/StatePlaying.cpp
+++ Source/Arkanoid/StatePlaying.cpp
@@ -8,6 +8,10 @@ using namespace std;
 
 StatePlaying::StatePlaying(Arkanoid * pArkanoid)
 	: ArkanoidState(pArkanoid)
+	, m_bForward(false)
+	, m_bBackward(false)
+	, m_bLeft(false)
+	, m_bRight(false)
 {
 }
 
@@ -17,16 +21,47 @@ StatePlaying::~StatePlaying()
 
 void StatePlaying::onUpdate(float fDT)
 {
-
+	
 }
 
 void StatePlaying::onKeyPressed(KeyboardKey eKey)
 {
 	switch (eKey)
 	{
+		case KEY_W:
+			m_bForward = true;
+			break;
+		case KEY_S:
+			m_bBackward = true;
+			break;
+		case KEY_A:
+			m_bLeft = true;
+			break;
+		case KEY_D:
+			m_bRight = true;
+			break;
 		case KEY_M:
 			getApp()->stopGame(); 
 			return;  
+			break;
+	}
+}
+
+void StatePlaying::onKeyReleased(KeyboardKey eKey)
+{
+	switch (eKey)
+	{
+		case KEY_W:
+			m_bForward = false;
+			break;
+		case KEY_S:
+			m_bBackward = false;
+			break;
+		case KEY_A:
+			m_bLeft = false;
+			break;
+		case KEY_D:
+			m_bRight = false;
 			break;
 	}
 }
--- Source/Arkanoid/StatePlaying.h
+++ Source/Arkanoid/StatePlaying.h
@@ -17,9 +17,15 @@ private:
 	// InputListener overrides
 	// {
 	virtual void onKeyPressed(KeyboardKey eKey) override;
+	virtual void onKeyReleased(KeyboardKey eKey) override;
 	virtual void onCharEntered(char ch) override;
 	virtual void onMBPressed(MouseButton eButton) override {}
 	virtual void onMBReleased(MouseButton eButton) override {}
 	virtual void onMouseMoved(int iX, int iY) override {}
 	// }
+
+	bool m_bForward;
+	bool m_bBackward;
+	bool m_bLeft;
+	bool m_bRight;
 };
