47462bd18e112ceca8034fe5a1483663a1d0b529
Hanno <hcombrinck@gmail.com>
2018-02-06 14:42:47 +0200

    Made Entity-World interface private and added comments

--- Source/Engine/Entity/Entity.h
+++ Source/Engine/Entity/Entity.h
@@ -11,18 +11,24 @@ namespace entity
 		Entity();
 		virtual ~Entity();
 
-		void init(EngineSystems& rEngine);
-
-		// TODO: Add several entitysystem updates (prePhysics, postPhysics, preVisuals, postVisuals etc.)
-		void update(float fDT);
-
 	protected:
 		EngineSystems& engine();
 
 	private:
+		// World interface
+		// {
+		friend class World;
+		void init(EngineSystems& rEngine);
+		// TODO: Add several entitysystem updates (prePhysics, postPhysics, preVisuals, postVisuals etc.)
+		void update(float fDT);
+		// }
+
+		// Entity events (to be overridden by derived entities)
+		// {
 		virtual void onInit() {}
 		// TODO: Add several entitysystem onUpdates (onPrePhysics, onPostPhysics, onPreVisuals, onPostVisuals etc.)
 		virtual void onUpdate(float fDT) {}
+		// }
 
 		EngineSystems* m_pEngine;
 	};
