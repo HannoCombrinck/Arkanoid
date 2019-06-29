1f8f1ed063cff030f358e46a5997923353785986
Hanno Combrinck <hcombrinck@gmail.com>
2019-06-29 11:37:45 +0200

    Moved socket test code to Socket.h and cleaned main.cpp.

--- Source/Engine/Network/Socket.h
+++ Source/Engine/Network/Socket.h
@@ -10,6 +10,42 @@ namespace network
 
 namespace network
 {
+	/*
+	// Example usage
+	void testSocket()
+	{
+		Socket socket(3001);
+		if (!socket.isOpen())
+		{
+			cout << "Failed to open socket\n";
+			return;
+		}
+
+		const char aData[] = "Test packet!";
+
+		// Send packet to self
+		socket.send(Address(3001), aData, sizeof(aData));
+
+		// Receive packet from self
+		Address senderAddress;
+		const auto uMaxPacketSize = 256U;
+		uchar aBuffer[uMaxPacketSize];
+		int iBytes = 1;
+		while (iBytes > 0)
+		{
+			iBytes = socket.receive(senderAddress, aBuffer, uMaxPacketSize);
+
+			if (iBytes > 0)
+			{
+				cout << "Packet data:\n";
+				for (auto i = 0; i < iBytes; ++i)
+					cout << aBuffer[i];
+				cout << endl;
+			}
+		}
+	}
+	*/
+
 	class Socket
 	{
 	public:
